b8d90c4191ea87faeeec9aaf4894d692a89ace9f
tizzer <7758258eheh@sina.com>
2018-08-19 13:34:33 +0800

    update

--- src/org/tizzer/pdfreader/util/WPS2PDF.java
+++ src/org/tizzer/pdfreader/util/WPS2PDF.java
@@ -18,12 +18,12 @@ class WPS2PDF {
      */
     static void word2pdf(String srcFilePath, String pdfFilePath) {
         ActiveXComponent activeXComponent = null;
-        Dispatch doc = null;
+        Dispatch wps = null;
         try {
             ComThread.InitSTA();
             activeXComponent = new ActiveXComponent("KWPS.Application");
             activeXComponent.setProperty("Visible", false);
-            Dispatch docs = activeXComponent.getProperty("Documents").toDispatch();
+            Dispatch kwps = activeXComponent.getProperty("Documents").toDispatch();
             Object[] obj = new Object[]{
                     srcFilePath,
                     new Variant(false),
@@ -31,14 +31,14 @@ class WPS2PDF {
                     new Variant(false),
                     new Variant("pwd")
             };
-            doc = Dispatch.invoke(docs, "Open", Dispatch.Method, obj, new int[1]).toDispatch();
-            Dispatch.put(doc, "RemovePersonalInformation", false);
-            Dispatch.call(doc, "ExportAsFixedFormat", pdfFilePath, WORD_TO_PDF_OPERAND);
+            wps = Dispatch.invoke(kwps, "Open", Dispatch.Method, obj, new int[1]).toDispatch();
+            Dispatch.put(wps, "RemovePersonalInformation", false);
+            Dispatch.call(wps, "ExportAsFixedFormat", pdfFilePath, WORD_TO_PDF_OPERAND);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (doc != null) {
-                Dispatch.call(doc, "Close", false);
+            if (wps != null) {
+                Dispatch.call(wps, "Close", false);
             }
             if (activeXComponent != null) {
                 activeXComponent.invoke("Quit", 0);
@@ -55,24 +55,24 @@ class WPS2PDF {
      */
     static void ppt2pdf(String srcFilePath, String pdfFilePath) {
         ActiveXComponent activeXComponent = null;
-        Dispatch ppt = null;
+        Dispatch wpp = null;
         try {
             ComThread.InitSTA();
             activeXComponent = new ActiveXComponent("KWPP.Application");
-            Dispatch ppts = activeXComponent.getProperty("Presentations").toDispatch();
+            Dispatch kwpp = activeXComponent.getProperty("Presentations").toDispatch();
             /*
              * call
              * param 4: ReadOnly
              * param 5: Untitled指定文件是否有标题
              * param 6: WithWindow指定文件是否可见
              * */
-            ppt = Dispatch.call(ppts, "Open", srcFilePath, true, true, false).toDispatch();
-            Dispatch.call(ppt, "SaveAs", pdfFilePath, PPT_TO_PDF_OPERAND);
+            wpp = Dispatch.call(kwpp, "Open", srcFilePath, true, true, false).toDispatch();
+            Dispatch.call(wpp, "SaveAs", pdfFilePath, PPT_TO_PDF_OPERAND);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (ppt != null) {
-                Dispatch.call(ppt, "Close");
+            if (wpp != null) {
+                Dispatch.call(wpp, "Close");
             }
             if (activeXComponent != null) {
                 activeXComponent.invoke("Quit");
@@ -89,34 +89,32 @@ class WPS2PDF {
      */
     static void excel2pdf(String inFilePath, String outFilePath) {
         ActiveXComponent activeXComponent = null;
-        Dispatch excel = null;
+        Dispatch et = null;
         try {
             ComThread.InitSTA();
             activeXComponent = new ActiveXComponent("KET.Application");
             activeXComponent.setProperty("Visible", new Variant(false));
             activeXComponent.setProperty("AutomationSecurity", new Variant(3)); // 禁用宏
-            Dispatch excels = activeXComponent.getProperty("Workbooks").toDispatch();
+            Dispatch ket = activeXComponent.getProperty("Workbooks").toDispatch();
 
             Object[] obj = new Object[]{
                     inFilePath,
                     new Variant(false),
                     new Variant(false)
             };
-            excel = Dispatch.invoke(excels, "Open", Dispatch.Method, obj, new int[9]).toDispatch();
-
+            et = Dispatch.invoke(ket, "Open", Dispatch.Method, obj, new int[9]).toDispatch();
             // 转换格式
             Object[] obj2 = new Object[]{
                     new Variant(EXCEL_TO_PDF_OPERAND), // PDF格式=0
                     outFilePath,
                     new Variant(0)  //0=标准 (生成的PDF图片不会变模糊) ; 1=最小文件
             };
-            Dispatch.invoke(excel, "ExportAsFixedFormat", Dispatch.Method, obj2, new int[1]);
-
+            Dispatch.invoke(et, "ExportAsFixedFormat", Dispatch.Method, obj2, new int[1]);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (excel != null) {
-                Dispatch.call(excel, "Close", new Variant(false));
+            if (et != null) {
+                Dispatch.call(et, "Close", new Variant(false));
             }
             if (activeXComponent != null) {
                 activeXComponent.invoke("Quit", new Variant[]{});
